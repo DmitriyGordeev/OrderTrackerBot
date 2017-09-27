4927ba35a9b5177073baa53d38d562ad2a7f2d3b
Weekend Master <weekend@super.com>
2017-09-27 20:43:43 +0300

    refactoreda little bit

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -15,6 +15,7 @@ import java.util.Date;
 
 public class Bot extends TelegramLongPollingBot {
 
+    // Uploads document:
     private void sendDocUploadingAFile(Long chatId, java.io.File save,String caption) throws TelegramApiException {
 
         SendDocument sendDocumentRequest = new SendDocument();
@@ -24,63 +25,72 @@ public class Bot extends TelegramLongPollingBot {
         sendDocument(sendDocumentRequest);
     }
 
+    private void handleFile(String filename, String record) {
+
+        String fileContent = "";
+        try {
+            fileContent = Fileio.readfile(filename);
+            fileContent = fileContent + record;
+            Fileio.writefile(filename, fileContent);
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            try {
+                Fileio.writefile(filename, record);
+            }
+            catch(IOException exc) {
+                exc.printStackTrace();
+            }
+        }
+    }
+
     public void onUpdateReceived(Update update) {
 
         if (update.hasMessage() && update.getMessage().hasText()) {
 
-            /// Std staff:
-            System.out.println(update.getMessage().getText());
-
             String username = update.getMessage().getChat().getUserName();
-            long userId   = update.getMessage().getChat().getId();
             String message_text = update.getMessage().getText();
+            long userId   = update.getMessage().getChat().getId();
             long chat_id = update.getMessage().getChatId();
 
+            String response = "";
 
-
-
-
-            /// Reading associated file:
+            // Reading associated file:
             DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             DateFormat dateFormatDateOnly = new SimpleDateFormat("yyyy-MM-dd");
             Date date = new Date();
 
+            String table_filename_noext = dateFormatDateOnly.format(date);
 
-            // get file command:
-            if(message_text.equals("/getfile")) {
-                File file = new File(dateFormatDateOnly.format(date) + ".csv");
+            // '/getfile' command
+            // sending .csv table if requested and if it exists
+            if(message_text.equals("/getfile"))
+            {
+                File file = new File(table_filename_noext + ".csv");
                 if(file.exists()) {
                     try {
-                        sendDocUploadingAFile(chat_id, file, "Статистика за сегодня:\n ");
+                        sendDocUploadingAFile(chat_id, file, "Статистика за " + table_filename_noext + ":\n ");
+                        return;
                     }
                     catch(TelegramApiException e) { e.printStackTrace(); }
                 }
+                else {
+                    response = "На " + table_filename_noext + " файла не существует";
+                    return;
+                }
             }
 
 
+            // make record into csv table:
             String record = userId + ";" + username + ";" + message_text + ";" + dateFormat.format(date) + "\n";
+            handleFile(table_filename_noext + ".csv", record);
+            response = "Записал";
 
-            String fileContent = "";
-            try {
-                fileContent = Fileio.readfile(dateFormatDateOnly.format(date) + ".csv");
-                fileContent = fileContent + record;
-                Fileio.writefile(dateFormatDateOnly.format(date) + ".csv", fileContent);
-            }
-            catch(IOException e) {
-                e.printStackTrace();
-                try {
-                    Fileio.writefile(dateFormatDateOnly.format(date) + ".csv", record);
-                }
-                catch(IOException exc) {
-                    exc.printStackTrace();
-                }
-            }
 
-            message_text += "\n username: " + username + " \n userId: " + userId;
-
+            // send response to user:
             SendMessage message = new SendMessage()
                     .setChatId(chat_id)
-                    .setText(message_text);
+                    .setText(response);
             try {
                 sendMessage(message);
             } catch (TelegramApiException e) {
